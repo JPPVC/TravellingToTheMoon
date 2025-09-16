4aff7560519bb3b864e218d687c091af277d150e
JPPVC <joavic@alunos.utfpr.edu.br>
2025-09-16 15:19:13 -0300

    update agcscript

--- agcscript/head/agcs/agcscript.1.hpp
+++ agcscript/head/agcs/agcscript.1.hpp
@@ -12,9 +12,35 @@
 
 namespace agcs {
 
+typedef class TokenStream TokenStreamS;
+typedef struct TokenStruct Token;
+
+const int maxArgQuant = 4;
+
+//ad -> add 
+//ads -> add value to storage
+//aug -> increment one to value in memory
+//bzf -> branch to fixed place in memory
+//bzmf -> branch to fixed place in memory if accumulator register is 0
+//ca , cae, caf -> mov conts of a mem location to the accumulator register
+//ccs -> get value of memory store in acc register and jump by comparing
+//com , -> (ver mais sobre)
+//cs -> pegar e converter valor de um local de mem usando 1's complement e colocar no registro acc
+//das -> dp add operation A,L a um par na memoria o que forma uma operacao dp + dp
+//dca -> dp mover conteudo de par A,L para um local da memoria movendo dp para dp dp -> dp
+//dcom -> not operation in the register pair A,L
+//dcs -> mover o negativo de do valor dp representado pelo par A,L em um outro local da memoria
+//ddoubl -> adicionar A,L a si mesmo
+//dim -> decrementar de valor positivo na memoria ou incrementar valor negatiov
+//double -> adiciona o registro A a si mesmo
+//dtcb -> operacao relacionada os banks e efetua um jump
+//
+const std::array<std::string, 46> InstructionMatch = {
+	"ad", "ads", "aug", "bzf", "bzmf", "ca", "ccs", "com"
+}
 
 enum KeyTypeID { //quantity of args in other words child nodes
-	ARG_1, ARG_2, ARG_3, UNVALID_TYPE = -1
+	ARG_0, ARG_1, ARG_2, ARG_3, UNVALID_TYPE = -1, INSTRUCTION_ARG
 };
 
 enum KeyID { //the ID 
@@ -22,23 +48,148 @@ enum KeyID { //the ID
  	 IF, WHILE, ELSE, ELIF, //Tokens representing flow control and conditional branching
          BODY, EXPRESSION,
          ROUTINE, MACRO, NAME,
+	 EXPR_ARG, INSTRUCTION,
 	 UNVALID_ID = -1
 };
 
-struct AstNode {
+class AstNode {
+
+	public:
+	Token token;
 	std::vector<int> childrenIndex;
 	int parentIndex;
+	AstNode(Token token, std::vector<int> childrenIndex, int parentIndex) : token(token), childrenIndex(childrenIndex), parentIndex(parentIndex){}
+	bool hasChild(int index) {
+		for (int c=0; c<childrenIndex.size(); c++) {
+			if (index == childrenIndex[c]) return true;
+		}
+		return false;
+	}
+};
+
+class Ast {
+
+	public:
+	std::vector<AstNode> nodes;
+	Ast(){}
+	
+	int addNode(AstNode node) {
+		nodes.push_back(node);
+		return nodes.size();
+	}
+	int addChildToNode(AstNode add, int to) {
+		nodes.push_back(add);
+		if (to < nodes.size()) {
+			std::cout << "ILLEGAL node index detected" << std::endl;
+			return -1;
+		}
+		nodes[to].childrenIndex.push_back(nodes.size());
+		return nodes.size();
+	}
+	void addChildToNode(int add, int to) {
+		if (add > nodes.size() || to > nodes.size()) {
+			std::cout << "ILLEGAL node index detected" << std::endl;
+			return;
+		}
+		if (!nodes[to].childrenIndex.hasChild(add)) {
+			nodes[to].childrenIndex.push_back(add);
+		} else {
+			std::cout << "ILLEGAL node index already added as child" << std::endl;
+			return;
+		}
+	}
 };
 
 class Keywords {
+	private:
+	TokenStreamS *ref;
+
 	public:
-	bool found;
+
+	bool found; 
+	Keywords(TokenStreamS *ref) : ref(ref) {}
 	virtual int typeId() {return KeyTypeID::UNVALID_TYPE;};
 	virtual int id() {return KeyID::UNVALID_ID;}
 	virtual bool searchForWord(std::string src) {return false;};
 	virtual int argQuant() {return -1;}; //quantity of args the keyword needs
 	virtual bool isArg(std::string src) {return false;};
 	virtual std::string toStr() {return "EMPTY KEYWORD";};
+	virtual std::string word() {return "__UNVALID__";};
+	virtual std::vector<Token> findArgs(std::regex expr) {};
+};
+
+class InstructionWord : public Keywords, public AstNode {
+	public:
+	virtual int typeId() {
+		return KeyTypeID::INSTRUCTION_ARG;
+	}
+	virtual int id() {
+		return KeyID::INSTRUCTION;
+	}
+	virtual bool searchForWord(std::string src) {
+		
+	}
+};
+
+class RoutineWord : public Keywords, public AstNode {
+	public:
+	virtual int typedId()override {
+		return KeyTypeID::ARG_3;
+	}
+	virtual int id()override {
+		return KeyID::ROUTINE;
+	}
+	virtual std::vector<Token> findArgs(std::string str, std::regex expr) override{
+		std::vector<Token> send;
+		std::smatch match;
+		
+		std::regex_search(src, match, expr);
+		
+		for (int c=0; c<match.size(); c++) {
+			send.push_back(Token {KeyTypeID::ARG_0, KeyID::EXPR_ARG} );
+		}
+		return send;
+		
+	}
+	virtual bool searchForWord(std::string src)override {
+
+		if (!syntaxAnalyze(src)) {
+			std::cout << "SYNTAX ERROR WHILE declaring routine" << std::endl
+			return false;
+		}
+
+		
+		std::vector<Token> args = findArgs(src, std::regex("(?<=\().*(?=,).*(?=\))"));
+	
+		if (args[0].id == KeyID::UNVALID_ID) {
+			std::cout << "Unable to find args for 'rot' keyword..." << std::endl;
+			return false;
+		}
+		
+		
+		return true;
+	}
+	virtual int argQuant()override {
+		return 3; //tag, args, body
+	}
+	bool syntaxAnalyze(std::string src) {
+
+		if (std::regex_search(src, std::regex("rot")) {
+			if (std::regex_search(src, std::regex("(?<=\().*(?=\))(.*(?=\{))"))) { //test routing arglist and opening bracket
+				if (std::regex_search(src, std::regex("(?<=\().*(?=,).*(?=\))")) || std::regex_search(src, std::regex(""))) {// test if the args are correctly typed
+				return true;
+				}else {
+					std::cout << "wrong formating in rot" << std::endl;
+				}
+			} else {
+				std::cout << "wrong formating in rot" << std::endl;
+			}
+		}
+		return false;
+	}
+	virtual bool isArg(std::string src)override {
+		return std::s;
+	}
 };
 
 
@@ -49,12 +200,7 @@ class MacroWord : public Keywords,  public AstNode {
 	std::vector<int> argsIndex;
 
 	bool searchForWord(std::string src)override {
-                if(std::regex_search(src, std::regex("@macro"))) {
-                       // std::cout << "true for @macro" << std::endl;
-			return true;
-                }
-                
-        	return false;
+                return std::regex_search(src, std::regex("@macro"));
 	}
 	
 	virtual int typeId() override {
@@ -72,35 +218,36 @@ class MacroWord : public Keywords,  public AstNode {
 		return std::regex_match(str, std::regex("(?<=\").*(?=\")")); //is an arg around double quotes
 	}
 
-	virtual std::string toStr() {
+	virtual std::string toStr()override {
 		return "MACRO WORD";
+	}
+	virtual std::string word()override {
+		return "@macro";
 	}
 };
 
 
-class Ast {
 
-};
-
-
-struct Token {
+struct TokenStruct {
 	int typeID, id, argQuant;
-	Token(int typeID, int id, int argQuant) : typeID(typeID), id(id), argQuant(argQuant) {}
+	std::string word;
+	TokenStruct(int typeID, int id, int argQuant, std::string word) : typeID(typeID), id(id), argQuant(argQuant), word(word) {}
 };
 
 class TokenStream {
 
 	private:
 
-	std::vector<Token> stream;
 	public:
+	std::vector<Token> stream;
 	TokenStream() {}
 	void charToTokens(std::string src) {
 		//std::string remove = src;
 		
 		std::vector<std::unique_ptr<Keywords>> keywords;
-		keywords.push_back(std::make_unique<MacroWord>(MacroWord{}));
-		
+		keywords.push_back(std::make_unique<MacroWord>(MacroWord{this}));
+		keywords.push_back(std::make_unique<RoutineWord>(RoutineWord{this}));
+
 		//std::cout <<  "before loop" << std::endl;
 		
 		for (int c=0; c<keywords.size(); c++) {
@@ -108,7 +255,7 @@ class TokenStream {
 			if (keywords[c]->searchForWord(src)) {
 				std::cout <<  "found" << std::endl;
 				
-				stream.push_back(Token {keywords[c]->typeId(), keywords[c]->id(), keywords[c]->argQuant()});
+				stream.push_back(Token {keywords[c]->typeId(), keywords[c]->id(), keywords[c]->argQuant(), keywords[c]->word()});
 			}
 		}
 	}
